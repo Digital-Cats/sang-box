fb1be65835d5793176342fe9c908c8e97e69f533
Roker2 <dimaminko2000@gmail.com>
2025-06-14 23:17:42 +0200

    CMake: Rename app

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
-        const QString baseName = "qsing-box_" + QLocale(locale).name();
+        const QString baseName = QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             app.installTranslator(&translator);
             break;
