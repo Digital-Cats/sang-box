6c1a62c36ba3d02e0c87032f51f970f1b5f66dd5
Roker2 <dimaminko2000@gmail.com>
2025-08-13 11:12:21 +0200

    Init ConfigDownloader

---
+++ src/config/config_downloader.cpp
@@ -0,0 +1,47 @@
+#include "config_downloader.h"
+
+#include <QObject>
+#include <QDebug>
+
+namespace config {
+
+ConfigDownloader::ConfigDownloader(const QUrl &url)
+    : m_manager(std::make_unique<QNetworkAccessManager>())
+    , m_request(url)
+    , m_reply(nullptr)
+{
+    m_request.setRawHeader("User-Agent", "sang-box");
+    m_reply = m_manager->get(m_request);
+    m_reply->ignoreSslErrors();
+    QObject::connect(m_reply, &QNetworkReply::finished, this, &ConfigDownloader::finished);
+    QObject::connect(m_reply, &QNetworkReply::errorOccurred,
+                     this, &ConfigDownloader::errorOccurred);
+    QObject::connect(m_reply, &QNetworkReply::downloadProgress, this, &ConfigDownloader::downloadProgress);
+    connect(m_reply, &QNetworkReply::sslErrors,
+            this, [](const QList<QSslError> &errors){ qDebug() << errors.empty(); });
+}
+
+bool ConfigDownloader::isFinished() const
+{
+    return m_reply->isFinished();
+}
+
+bool ConfigDownloader::isRunning() const
+{
+    return m_reply->isRunning();
+}
+
+QNetworkReply::NetworkError ConfigDownloader::error() const
+{
+    return m_reply->error();
+}
+
+QString ConfigDownloader::getConfig() const
+{
+    if (!isFinished())
+        return {};
+    QByteArray data = m_reply->readAll();
+    return QString(data);
+}
+
+}
---
+++ src/config/config_downloader.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+
+#include <memory>
+
+namespace config {
+
+class ConfigDownloader : public QObject
+{
+    Q_OBJECT
+
+    using QNetworkAccessManagerUPtr = std::unique_ptr<QNetworkAccessManager>;
+public:
+    ConfigDownloader(const QUrl &url);
+
+    bool isFinished() const;
+    bool isRunning() const;
+
+    QNetworkReply::NetworkError error() const;
+    QString getConfig() const;
+
+signals:
+    void finished();
+    void errorOccurred(QNetworkReply::NetworkError code);
+    void downloadProgress(qint64 bytesReceived, qint64 bytesTotal);
+
+private:
+    QNetworkAccessManagerUPtr m_manager;
+    QNetworkRequest m_request;
+    QNetworkReply *m_reply;
+};
+
+}
